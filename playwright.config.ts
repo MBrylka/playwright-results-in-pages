e3490064eb6ced2e9b4833ea32c4b16fd138e8a2
Maciej Brylka <maciejbrylka94@gmail.com>
2025-08-10 16:19:18 +0100

    fix pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['json', { outputFile: 'report.json' }],
+    ['json', { outputFile: 'playwright-report/report.json' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
